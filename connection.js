93b64fc1f59a8a436e51b2c5d21acb7a99b04e81
unknown <abubakarsarwar198@gmail.com>
2024-05-28 09:53:34 +0500

    running

--- connection.js
+++ connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 // Create Database Connection
 const connectToDatabase = async () => {
   try {
-    const connection = await mongoose.connect(process.env.DB_URI);
+    const connection = await mongoose.connect(process.env.MONGO_URI);
     console.log("connected Successfully!!");
   } catch (err) {
     console.log("connection failed");
